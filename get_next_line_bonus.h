7c03258267d7f2590b8f936948e6b573df059163
Will's Lullaby <100817004+dieremy@users.noreply.github.com>
2023-01-26 10:03:52 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 09:52:55 by robegarc          #+#    #+#             */
+/*   Updated: 2023/01/26 09:58:28 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
+char	*get_line(int fd, char *line);
+char	*get_new_line(char *line);
+char	*new_line(char *line);
+int		ft_strlen(char *s);
+
+#endif
